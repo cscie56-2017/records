7cfbca156c79eff02c1303705e7364e8a69f4692
mpkerry <mkerry@g.harvard.edu>
2017-02-09 18:49:04 -0500

    artistcontroller test

--- src/test/groovy/cscie56/section3/ArtistControllerSpec.groovy
+++ src/test/groovy/cscie56/section3/ArtistControllerSpec.groovy
@@ -9,10 +9,7 @@ class ArtistControllerSpec extends Specification {
 
     def populateValidParams(params) {
         assert params != null
-
-        // TODO: Populate valid properties like...
-        //params["name"] = 'someValidName'
-        assert false, "TODO: Provide a populateValidParams() implementation for this generated test suite"
+        params << [firstName:'firstName',lastname:'lastName',countryOfOrigin:'UK']
     }
 
     void "Test the index action returns the correct model"() {
